13bed7a617d3c080c1547c2993a52e8708232eee
Ibrahim Hafez <cursedlawliet@gmail.com>
2023-04-08 16:12:12 +0300

    Connect to redis db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Readex_Pro } from "next/font/google";
+import { db } from "@/lib/db";
 
 const inter = Readex_Pro({ subsets: ["latin", "arabic"] });
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hello", "hello");
   return <div className="text-red-500">Hello world from razer</div>;
 }
